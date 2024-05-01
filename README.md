ee71bf1449532fc475ad2d269fab09b7fcd1d116
Urvesh <urveshborkar@gmail.com>
2024-05-01 12:08:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Weather App
+
+Weather App is free weather api written using www.weatherapi.com/.
+
+## Contribution
+
+This is my first app of programming journy. Hope you like it. If you have any improvement ideas feel free to suggest or pull request.
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to manage.py file
+
+`API_KEY`
+
+Sign in to www.weatherapi.com and it will generate your free `API_KEY` now simply replace it in manage.py.
+```bash
+  API_KEY = "05d06c3336f44dfa91c70345242804"
+```
---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+
+print("Welcome to Weather App.")
+city = input("Enter the city name: ")
+
+# validate user input for options
+while True:
+    try:
+        i = int(input("What would you like to know: \n   1. The current temperatur. \n   2. The location details. \n   3. The current weather details. \nEnter the number: "))
+        if i in [1,2,3]:
+            break
+        else:
+            print("Invalidate option. Please select a number between 1 and 3.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+# Enter your API Key
+API_KEY = "05d06c3336f44dfa91c70345242804"
+url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+
+try:
+    r = requests.get(url)
+    wDict= json.loads(r.text)
+    t = (wDict["current"]["temp_c"])
+    loc = (wDict["location"])
+    curr = (wDict["current"])
+    if i==1:
+        print(f"Current temperatur of {city} is {t} °C.")
+    elif i==2: 
+        print(f"Location details: \n{loc}")
+    elif i==3:
+        print(f"Current weather: \n{curr}")
+    else:
+        print("Invalidate option selected.")
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
